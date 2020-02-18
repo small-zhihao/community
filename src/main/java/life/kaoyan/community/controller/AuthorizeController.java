31454a75446a2762800d53c63f28c9d79e7fc224
small-zhihao <944839605@qq.com>
2020-02-18 22:28:44 +0800

    add wentixianshi

--- src/main/java/life/kaoyan/community/controller/AuthorizeController.java
+++ src/main/java/life/kaoyan/community/controller/AuthorizeController.java
@@ -46,7 +46,7 @@ public class AuthorizeController {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        if (githubUser !=null){
+        if (githubUser !=null &&githubUser.getId() !=null){
             User user = new User();
             String token = UUID.randomUUID().toString();
             user.setToken(token);
@@ -54,6 +54,7 @@ public class AuthorizeController {
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
+            user.setAvatarUrl(githubUser.getAvatar_url());
             userMapper.insert(user);
             response.addCookie(new Cookie("token",token));
             return "redirect:/";
--- src/main/java/life/kaoyan/community/controller/IndexController.java
+++ src/main/java/life/kaoyan/community/controller/IndexController.java
@@ -1,31 +1,44 @@
 package life.kaoyan.community.controller;
 
+import life.kaoyan.community.dto.QuestionDTO;
+import life.kaoyan.community.mapper.QuestionMapper;
 import life.kaoyan.community.mapper.UserMapper;
+import life.kaoyan.community.model.Question;
 import life.kaoyan.community.model.User;
+import life.kaoyan.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class IndexController {
     @Autowired
     private UserMapper userMapper;
+    @Autowired
+    private QuestionService questionService;
     @GetMapping("/")
-    public  String index(HttpServletRequest request){
+    public  String index(HttpServletRequest request,
+                         Model model){
         Cookie[] cookies = request.getCookies();
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals("token")){
-                String token = cookie.getValue();
-                User user =userMapper.findByToken(token);
-                if (user !=null){
-                    request.getSession().setAttribute( "user",user);
+        if (cookies!=null &&cookies.length!=0){
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")){
+                    String token = cookie.getValue();
+                    User user =userMapper.findByToken(token);
+                    if (user !=null){
+                        request.getSession().setAttribute( "user",user);
+                    }
+                    break;
                 }
-                break;
             }
         }
+        List<QuestionDTO> questionList=questionService.list();
+        model.addAttribute("questions",questionList);
         return "index";
     }
 }
