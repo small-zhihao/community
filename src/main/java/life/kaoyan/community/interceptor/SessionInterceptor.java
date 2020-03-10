6293d240369018f69f8a2cd3a0459d0085ec6e78
small-zhihao <944839605@qq.com>
2020-03-10 14:06:44 +0800

    add guifanhuawentimiaoshu

--- src/main/java/life/kaoyan/community/interceptor/SessionInterceptor.java
+++ src/main/java/life/kaoyan/community/interceptor/SessionInterceptor.java
@@ -3,6 +3,7 @@ package life.kaoyan.community.interceptor;
 import life.kaoyan.community.mapper.UserMapper;
 import life.kaoyan.community.model.User;
 import life.kaoyan.community.model.UserExample;
+import life.kaoyan.community.service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -17,6 +18,8 @@ import java.util.List;
 public class SessionInterceptor implements HandlerInterceptor {
     @Autowired
     private UserMapper userMapper;
+    @Autowired
+    private NotificationService notificationService;
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         Cookie[] cookies = request.getCookies();
@@ -30,6 +33,8 @@ public class SessionInterceptor implements HandlerInterceptor {
                     List<User> users = userMapper.selectByExample(userExample);
                     if (users.size() !=0){
                         request.getSession().setAttribute( "user",users.get(0));
+                        Long unreadCount=notificationService.unreadCount(users.get(0).getId());
+                        request.getSession().setAttribute("unreadCount",unreadCount);
                     }
                     break;
                 }
