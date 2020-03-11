9751b1744568f41c60bfb341bd36b359318da075
small-zhihao <944839605@qq.com>
2020-03-11 14:07:50 +0800

    add ufile

---
+++ src/main/java/life/kaoyan/community/controller/FileController.java
@@ -0,0 +1,34 @@
+package life.kaoyan.community.controller;
+
+import life.kaoyan.community.dto.FileDTO;
+import life.kaoyan.community.provider.UCloudProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Controller
+public class FileController {
+    @Autowired
+    private UCloudProvider uCloudProvider;
+    @RequestMapping("/file/upload")
+    @ResponseBody
+    public FileDTO upload(HttpServletRequest request){
+        MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+        MultipartFile file = multipartRequest.getFile("editormd-image-file");
+        try {
+            uCloudProvider.upload(file.getInputStream(),file.getContentType(),file.getOriginalFilename());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        FileDTO fileDTO = new FileDTO();
+        fileDTO.setSuccess(1);
+        fileDTO.setUrl("/images/wechat.png");
+        return fileDTO;
+    }
+}
