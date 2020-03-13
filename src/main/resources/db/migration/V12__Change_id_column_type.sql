9f29c9957f7629cac116af72735a45e31f893091
small-zhihao <944839605@qq.com>
2020-03-13 15:17:06 +0800

    修复数据库

--- src/main/resources/db/migration/V12__Change_id_column_type.sql
+++ src/main/resources/db/migration/V12__Change_id_column_type.sql
@@ -1,2 +1,2 @@
-alter table QUESTION alter column ID  AUTO_INCREMENT not null;
-alter table USER alter column ID AUTO_INCREMENT  not null ;
+alter table QUESTION alter column ID BIGINT AUTO_INCREMENT not null;
+alter table USER alter column ID BIGINT AUTO_INCREMENT  not null ;
